fd39a8a826789e074cf6bba6bc4221adbf1699cf
Ben Zhang <benzhangniu@gmail.com>
2020-03-29 15:03:01 -0400

    Add sys_symlinks for system symlinks (requires root)

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,9 @@
 import argparse
 import logging
-from .dcconfig import DCConfig
+import sys
+import os
+import subprocess
+from .dcconfig import DCConfig, Scope
 from .dcutils import str2bool
 from .version import __version__
 
@@ -14,18 +17,37 @@ def main():
     parser.add_argument('filenames', metavar='filename', type=str, nargs='+',
                         help='The name of a configuration files')
     parser.add_argument('--version', action='version', version='%(prog)s {}'.format(__version__))
+    parser.add_argument('--sys-only', action='store_true')
+    parser.add_argument('--user-only', action='store_true')
 
     args = parser.parse_args()
 
     # TODO: figure out how to add verbose logging option (--verbose, -v)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
+
+    has_system_config = False
 
     for filename in args.filenames:
         config = DCConfig.from_yaml(filename)
-        config.apply()
+        if args.sys_only:
+            config.apply(scope=Scope.SYS)
+        elif args.user_only:
+            config.apply(scope=Scope.USER)
+        elif not config.requires_root() or os.geteuid() == 0:
+            config.apply(scope=Scope.ALL)
+        else:
+            config.apply(scope=Scope.USER)
+            has_system_config = True
 
-    logger.info('Done!')
+    if has_system_config and not args.user_only:
+        print("Enter password here to apply system config:")
+        subprocess_env = os.environ.copy()
+        subprocess_env['DOTCONF_SUBPROCESS'] = 'true'
+        subprocess.call(['sudo', '-E', sys.executable] + sys.argv + ['--sys-only'], env=subprocess_env)
+        subprocess.call(['sudo', '-k']) # revoke sudo
 
+    if os.environ.get('DOTCONF_SUBPROCESS') is None:
+        logger.info('Done!')
 
 if __name__ == "__main__":
     main()
--- src/dcconfig.py
+++ src/dcconfig.py
@@ -5,44 +5,75 @@ import os
 import shutil
 import time
 from collections import namedtuple
+from enum import Enum
 from pathlib import Path
-from strictyaml import load as load_yaml, Map, MapPattern, Str, Int, Seq, YAMLError
+from strictyaml import load as load_yaml, Map, MapPattern, Str, Int, Bool, Optional, Seq, EmptyDict, YAMLError
 from typing import NamedTuple, Dict, Union, Sequence
 from .dcutils import absp
+
+
+class Scope(Enum):
+    ALL = 0
+    USER = 1
+    SYS = 2
 
 
 logger = logging.getLogger('dot-conf')
 # this is a redeclaration of the schema of DCConfig.
 #   https://github.com/crdoconnor/strictyaml/issues/90
 YAMLSchema = Map({"backup_directory": Str(),
-                  "symlinks": MapPattern(Str(), Str() | Seq(Str()))})
-
+                  Optional("symlinks", default={}): MapPattern(Str(), Str() | Seq(Str())) | EmptyDict(),
+                  Optional("sys_symlinks", default={}): MapPattern(Str(), Str() | Seq(Str())) | EmptyDict(),
+                })
 
 class DCConfig():
     config_path: Path
     backup_directory: Path
     symlinks: Dict[Path, Union[Path, Sequence[Path]]]
+    sys_symlinks: Dict[Path, Union[Path, Sequence[Path]]]
 
-    def __init__(self, config_path, backup_directory, symlinks, **kwargs):
+    def __init__(self, config_path, backup_directory, symlinks, sys_symlinks, **kwargs):
         self.config_path = absp(config_path)
         self.backup_directory = absp(backup_directory)
         self.symlinks = dict([[absp(self.config_path.parent, src), [Path(d).expanduser() for d in dest] if isinstance(
             dest, list) else Path(dest).expanduser()] for src, dest in symlinks.items()])
+        self.sys_symlinks = dict([[absp(self.config_path.parent, src), [Path(d).expanduser() for d in dest] if isinstance(
+            dest, list) else Path(dest).expanduser()] for src, dest in sys_symlinks.items()])
 
-    def apply(self):
-        logger.info("Applying config: {}".format(self.config_path))
-        logger.info("Creating backup directory (if it does not exist): {}".format(
-            self.backup_directory))
-        self.backup_directory.mkdir(parents=True, exist_ok=True)
+    def requires_root(self):
+        return len(self.sys_symlinks) > 0
 
-        for src, dest_ in self.symlinks.items():
+    def apply(self, scope):
+        logger.info("Applying config ({}): {}".format(scope.name, self.config_path))
+        if not self.backup_directory.exists():
+            logger.info("Creating backup directory: {}".format(
+                self.backup_directory))
+            self.backup_directory.mkdir(parents=True, exist_ok=True)
+
+        if scope is Scope.USER:
+            self.apply_user_symlinks()
+        elif scope is Scope.SYS:
+            self.apply_sys_symlinks()
+        else:
+            self.apply_user_symlinks()
+            self.apply_sys_symlinks()
+
+
+    def apply_user_symlinks(self):
+        self._apply_symlinks(self.symlinks)
+
+    def apply_sys_symlinks(self):
+        self._apply_symlinks(self.sys_symlinks)
+
+    def _apply_symlinks(self, symlinks):
+        for src, dest_ in symlinks.items():
             dests = dest_ if isinstance(dest_, list) else [dest_]
             if not src.exists():
                 logger.info("{} does not exist, skipping".format(src))
                 continue
 
             for dest in dests:
-                logger.info(
+                logger.debug(
                     "Preparing to link {} -> {}".format(src, dest))
 
                 if dest.exists() or dest.is_symlink(): # exists() returns false for orphan symlinks

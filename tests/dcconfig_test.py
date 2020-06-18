725590a60d13569f4dbff849cd2dd9af221dd65f
Ben Zhang <benzhangniu@gmail.com>
2020-06-17 22:43:39 -0700

    Add tests for sys symlinks

--- tests/dcconfig_test.py
+++ tests/dcconfig_test.py
@@ -19,6 +19,7 @@ class TestDCConfig(TestCase):
     def setUp(self):
         self.setUpPyfakefs()
         self.fs.add_real_directory(fixture_path)
+        self.fs.create_dir('/etc')
 
     def test_loads_and_applies_basic_config(self):
         config = DCConfig.from_yaml(os.path.join(fixture_path, 'basic_config/basic_config.yaml'))
@@ -35,6 +36,44 @@ class TestDCConfig(TestCase):
         self.assertTrue(Path(Path.home(), '.bashrc').is_symlink())
         self.assertEqual(Path(Path.home(), '.bashrc').resolve(),
                          Path(fixture_path, 'basic_config', '.bashrc').resolve())
+        self.assertTrue(Path(Path.home(), '.config/backup').is_dir())
+        # wacky syntax from https://stackoverflow.com/a/54216885/4527337
+        self.assertFalse(any(Path(Path.home(), '.config/backup').iterdir()))
+
+    def test_loads_and_applies_sys_config(self):
+        config = DCConfig.from_yaml(os.path.join(fixture_path, 'sys_only/sys_only.yaml'))
+        self.assertEqual(config.backup_directory, absp('~/.config/backup'))
+        self.assertEqual(config.sys_symlinks.get(absp(
+            fixture_path, 'sys_only', '.vimrc')), absp('/etc/vimrc'))
+        self.assertEqual(config.sys_symlinks.get(absp(
+            fixture_path, 'sys_only', '.bashrc')), absp('/etc/bashrc'))
+        config.apply(Scope.SYS)
+
+        self.assertTrue(Path('/etc/vimrc').is_symlink())
+        self.assertEqual(Path('/etc/vimrc').resolve(),
+                         Path(fixture_path, 'sys_only', '.vimrc').resolve())
+        self.assertTrue(Path('/etc/bashrc').is_symlink())
+        self.assertEqual(Path('/etc/bashrc').resolve(),
+                         Path(fixture_path, 'sys_only', '.bashrc').resolve())
+        self.assertTrue(Path(Path.home(), '.config/backup').is_dir())
+        # wacky syntax from https://stackoverflow.com/a/54216885/4527337
+        self.assertFalse(any(Path(Path.home(), '.config/backup').iterdir()))
+
+    def test_loads_and_applies_mixed_config(self):
+        config = DCConfig.from_yaml(os.path.join(fixture_path, 'user_and_sys/user_and_sys.yaml'))
+        self.assertEqual(config.backup_directory, absp('~/.config/backup'))
+        self.assertEqual(config.symlinks.get(absp(
+            fixture_path, 'user_and_sys', '.vimrc')), absp('~/.vimrc'))
+        self.assertEqual(config.sys_symlinks.get(absp(
+            fixture_path, 'user_and_sys', '.bashrc')), absp('/etc/bashrc'))
+        config.apply(Scope.ALL)
+
+        self.assertTrue(Path(Path.home(), '.vimrc').is_symlink())
+        self.assertEqual(Path(Path.home(), '.vimrc').resolve(),
+                         Path(fixture_path, 'user_and_sys', '.vimrc').resolve())
+        self.assertTrue(Path('/etc/bashrc').is_symlink())
+        self.assertEqual(Path('/etc/bashrc').resolve(),
+                         Path(fixture_path, 'user_and_sys', '.bashrc').resolve())
         self.assertTrue(Path(Path.home(), '.config/backup').is_dir())
         # wacky syntax from https://stackoverflow.com/a/54216885/4527337
         self.assertFalse(any(Path(Path.home(), '.config/backup').iterdir()))
